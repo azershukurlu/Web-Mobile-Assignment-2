5e98b6f77f597cddac533fff6aba0526945dc933
azershukurlu <ashukurlu12315@ada.edu.az>
2023-12-05 02:34:03 +0400

    added all products

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 const apiUrl = "https://dummyjson.com/products";
 
-fetch(apiUrl)
-.then(response =>response.json())
-.then(data=>console.log(data.products));
+function getProducts() {
+    fetch(apiUrl).then(response => response.json()).then(data => createCard(data))
+        .catch(error => console.log('error:', error))
+}
+
+
+
+
+function createCard(data) {
+    const cardContainer = document.getElementById("card-container");
+    data.products.forEach(products => {
+        console.log(products.images[0])
+        cardContainer.innerHTML += `
+        <div class="card">
+            <div class="img">
+                <img src="${products.images[0]}" alt="Prodcut Name">
+            </div>
+            <h3>${products.title}</h3>
+            <p>Price: $${products.price}</p>
+            <a href="" class="detail-button">Details</a>
+        </div>`
+    });
+
+
+
+}
+
+getProducts();
