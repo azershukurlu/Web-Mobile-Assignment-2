7d5e82bcde2d5ae8f300661bee217898123a1dfa
azershukurlu <ashukurlu12315@ada.edu.az>
2023-12-05 12:44:23 +0400

    added product display

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 const apiUrl = "https://dummyjson.com/products";
 
-function getProducts() {
-    fetch(apiUrl).then(response => response.json()).then(data => createCard(data))
-        .catch(error => console.log('error:', error))
-}
+var datas = [];
 
-function createCard(data) {
-    const cardContainer = document.getElementById("card-container");
-    data.products.forEach(products => {
-        console.log(products)
-        cardContainer.innerHTML += `
+
+async function CardCreate(){
+    await productsDatas();
+
+    document.getElementById("card-container").innerHTML = datas.map(products=>
+    `
         <div class="card">
             <div class="img">
                 <img src="${products.images[0]}" alt="Prodcut Name">
             </div>
             <h3>${products.title}</h3>
-            <p>Price: $${products.price}</p>
+            <p>Category: ${products.category}</p>
+            <p>Price: ${products.price}</p>
+            <p>Old Price: ${products.price + (products.price*products.discountPercentage/100)}</p>
+            <p>Stock: ${products.stock}</p>
             <a href="" class="detail-button">Details</a>
-        </div>`
-    });
+        </div>
+    `
+    ).join("");
 }
+CardCreate();
 
-getProducts();
+async function productsDatas(){
+    const data = await fetch(apiUrl);
+    const response =await data.json();
+    datas = response.products;
+}
