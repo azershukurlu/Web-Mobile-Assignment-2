da4eb150e7b3b4a227b36c00dc72d29aedac52a0
azershukurlu <ashukurlu12315@ada.edu.az>
2023-12-05 19:28:53 +0400

    created detail page

--- main.js
+++ main.js
@@ -2,12 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const productList = document.getElementById('card-container');
     const searchInput = document.getElementById('searchBox')
     const categoryUl = document.getElementById('category')
+    const prevButton = document.getElementById('prevPage');
+    const nextButton = document.getElementById('nextPage');
+    const pageIndicator = document.getElementById('currentPage');
 
 
-
-
+    const itemsPerPage = 10;
+    let currentPage = 1;
+    let totalItems = 0;
 
     let productsData = []
+    prevButton.addEventListener('click', function () {
+        if (currentPage > 1) {
+            changePage(currentPage - 1);
+        }
+    });
+    nextButton.addEventListener('click', function () {
+        if (currentPage < Math.ceil(totalItems / itemsPerPage)) {
+            changePage(currentPage + 1);
+        }
+    });
 
     searchInput.addEventListener('keyup', function (event) {
         const searchTerm = event.target.value.toLowerCase();
@@ -17,16 +31,24 @@ document.addEventListener('DOMContentLoaded', function () {
             );
         displayProducts(filteredProducts)
     })
-    async function fetchProducts() {
+    async function fetchProducts(page = 1) {
 
-        const response = await fetch('https://dummyjson.com/products');
+        const response = await fetch(`https://dummyjson.com/products?page=${page}&limit=${itemsPerPage}`);
 
         const data = await response.json()
         productsData = data.products
         displayProducts(productsData)
-
+        updatePageIndicator();
 
     }
+    function changePage(page) {
+        currentPage = page;
+        fetchProducts(currentPage);
+    }
+    function updatePageIndicator() {
+        pageIndicator.textContent = `Page ${currentPage} of ${Math.ceil(totalItems / itemsPerPage)}`;
+    }
+
     async function fetchCategories() {
         const response = await fetch('https://dummyjson.com/products/categories');
         const data = await response.json()
@@ -46,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
             <p>Price: ${product.price}</p>
             <p>Old Price: ${product.price + (product.price * product.discountPercentage / 100)}</p>
             <p>Stock: ${product.stock}</p>
-            <a href="" class="detail-button">Details</a>
+            <button onclick="redirectToDetailPage(${product.id})" class="detail-button">Details</button>
              </div>
             
             `
@@ -71,8 +93,111 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     fetchCategories();
-    fetchProducts();
+    fetchProducts(currentPage);
 
-    
+
 
 });
+
+
+
+
+
+
+//detail
+function Detail(id) {
+    fetch(`https://dummyjson.com/products/${id}`)
+        .then(res => res.json())
+        .then(console.log);
+}
+
+function changeMainImage(thumbnail, imageSrc) {
+    var thumbnails = document.querySelectorAll('.thumbnail');
+    thumbnails.forEach(function (thumb) {
+        thumb.classList.remove('active');
+    });
+
+    thumbnail.classList.add('active');
+
+    var mainImage = document.getElementById("mainImage");
+
+    mainImage.src = imageSrc;
+
+}
+
+
+const productContainer = document.getElementById("product-container");
+
+function redirectToDetailPage(productId) {
+    window.location.href = 'detail.html?id=' + productId;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+    fetch('https://dummyjson.com/products/' + productId)
+        .then(response => response.json())
+        .then(productData => {
+            producDetailDisplay(productData);
+        })
+
+});
+
+function producDetailDisplay(data) {
+    console.log(data.images[0]);
+    productContainer.innerHTML = ` 
+    <div class="product">
+        <div class="col">
+            <div class="product_img_wrapper">
+                <img id="mainImage"
+                    src="${data.images[0]}"
+                    alt="Main Image">
+                <div id="imageContainer">
+                     <img src="${data.images[0]}"
+                        class="thumbnail active"
+                        onclick="changeMainImage(this, '${data.images[0]}')">
+                    <img src="${data.images[1]}"
+                        class="thumbnail"
+                        onclick="changeMainImage(this, '${data.images[1]}')">
+                    <img class="thumbnail" src="${data.images[2]}"
+                        onclick="changeMainImage(this, '${data.images[2]}')">
+                    <img src="${data.images[3]}" 
+                        class="thumbnail"
+                        onclick="changeMainImage(this, '${data.images[3]}')">
+                </div>
+                <span class="product_discount">-${data.discountPercentage}%</span>
+            </div>
+        </div>
+        <div class="col product_about_wrapper">
+            <h2 class="product_name">${data.title}</h2>
+            <div class="product_about">
+                <div class="product_price_rating_wrapper">
+                    <p class="product_price">$${Math.ceil(data.price - (data.price * data.discountPercentage / 100))} <del class="span-price">${data.price}</del></p>
+                    <span class="price_line"></span>
+                    <div class="product_rating_wrapper">
+                        <img class="star"
+                            src="https://upload.wikimedia.org/wikipedia/commons/4/44/Plain_Yellow_Star.png"
+                            alt="star">
+                        <p class="product_rating">${data.rating}</p>
+                    </div>
+                </div>
+                <p class="product_description">${data.description}</p>
+
+                <div class="brand_category_wrapper">
+                    <div class="brand">
+                        <span class="">Brand:</span>
+                        <span>${data.brand}</span>
+                    </div>
+                    <div class="category">
+                        <span class="">Category:</span>
+                        <span>${data.category}</span>
+                    </div>
+                </div>
+                <div class="add_to_cart_btn_wrapper">
+                    <span>Avaible in stock: ${data.stock}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+`
+}
