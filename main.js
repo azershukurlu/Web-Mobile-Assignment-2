8efec68141843a68f649bd21953e11add3504d9d
azershukurlu <ashukurlu12315@ada.edu.az>
2023-12-05 15:41:30 +0400

    added searchbox js and category js

--- main.js
+++ main.js
@@ -1,31 +1,74 @@
-const apiUrl = "https://dummyjson.com/products";
+document.addEventListener('DOMContentLoaded', function () {
+    const productList = document.getElementById('card-container');
+    const searchInput = document.getElementById('searchBox')
+    const categoryArea = document.getElementById('category')
+    let productsData = []
 
-var datas = [];
+    searchInput.addEventListener('keyup', function (event) {
+        const searchTerm = event.target.value.toLowerCase();
+        const filteredProducts = productsData.filter
+            (
+                product => product.title.toLowerCase().includes(searchTerm) || product.category.toLowerCase().includes(searchTerm)
+            );
+        displayProducts(filteredProducts)
+    })
+    async function fetchProducts() {
+
+        const response = await fetch('https://dummyjson.com/products');
+
+        const data = await response.json()
+        productsData = data.products
+        displayProducts(productsData)
 
 
-async function CardCreate(){
-    await productsDatas();
+    }
+    async function fetchCategories() {
+        const response = await fetch('https://dummyjson.com/products/categories');
+        const data = await response.json()
+        displayCategories(data)
+    }
 
-    document.getElementById("card-container").innerHTML = datas.map(products=>
-    `
-        <div class="card">
+    function displayProducts(products) {
+        productList.innerHTML = "";
+
+        products.forEach(product => {
+            productList.innerHTML += `
+            <div class="card">
             <div class="img">
-                <img src="${products.images[0]}" alt="Prodcut Name">
+                <img src="${product.images[0]}" alt="Prodcut Name">
             </div>
-            <h3>${products.title}</h3>
-            <p>Category: ${products.category}</p>
-            <p>Price: ${products.price}</p>
-            <p>Old Price: ${products.price + (products.price*products.discountPercentage/100)}</p>
-            <p>Stock: ${products.stock}</p>
+            <h3>${product.title}</h3>
+            <p>Category: ${product.category}</p>
+            <p>Price: ${product.price}</p>
+            <p>Old Price: ${product.price + (product.price * product.discountPercentage / 100)}</p>
+            <p>Stock: ${product.stock}</p>
             <a href="" class="detail-button">Details</a>
-        </div>
-    `
-    ).join("");
-}
-CardCreate();
+             </div>
+            
+            `
+        });
+    }
+    function displayCategories(categories) {
+        categories.forEach(category => {
+            categoryArea.innerHTML += `<li id="list-item">${category}</li>`
+            const listItem = document.getElementById('list-item')
+            listItem.addEventListener('click', async () => {
+                console.log("salam");
+                const productsResponse = await fetch(`https://dummyjson.com/products?category=${category}`)
+                const products = await productsResponse.json();
 
-async function productsDatas(){
-    const data = await fetch(apiUrl);
-    const response =await data.json();
-    datas = response.products;
-}
+                displayProducts(products);
+            })
+
+        })
+
+    }
+
+
+    fetchCategories();
+    fetchProducts();
+
+
+});
+
+
